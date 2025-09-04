f0e51fc5f8859f2c1a17ddd47f62ff59c95180c1
Mtn16 <mtn16@bloodbear.cz>
2025-09-04 21:30:24 +0200

    Fixed tab completer

--- src/main/java/cz/bloodbear/discordLink/core/utils/TabCompleterHelper.java
+++ src/main/java/cz/bloodbear/discordLink/core/utils/TabCompleterHelper.java
@@ -6,6 +6,9 @@ import java.util.List;
 public class TabCompleterHelper {
     public static List<String> getArguments(List<String> choices, String argument) {
         List<String> arguments = new ArrayList<>();
+        if (argument.isEmpty()) {
+            return choices;
+        }
         choices.forEach(choice -> {
             if(choice.toLowerCase().startsWith(argument.toLowerCase())) {
                 arguments.add(choice);
